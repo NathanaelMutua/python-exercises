c2aa57ba45e0e9777f3e208f6f8eb60752d3465d
Nxsh <nathanael.mutua.m@gmail.com>
2025-05-26 21:25:07 +0300

    add initial commit

---
+++ list-exercise.py
@@ -0,0 +1,3 @@
+s = 3, 4, 2, 45, 67
+r = 'conglomorate'
+print(r[1:9:s[2]])
---
+++ version-checker.py
@@ -0,0 +1,9 @@
+import sys, platform
+
+print("Using sys module...")
+
+print('Python version: ', sys.version, '\n', sys.version_info )
+
+print("Also, using platform module...")
+
+print('Python version: ', platform.python_version_tuple())
